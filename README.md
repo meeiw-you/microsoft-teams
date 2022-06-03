86945988df9aec07daef192e67356a37977fb3e2
mei you <meiaqqff@protonmail.com>
2022-06-03 14:21:25 +0300

    add install.sh and readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# microsoft teams
+1)chmod +x install.sh
+2)./install.sh
+3)When you write teams to your terminal, it is over, you have set up
---
+++ install.sh
@@ -0,0 +1,6 @@
+
+cd ~/Downloads
+wget https://go.microsoft.com/fwlink/p/?LinkID=2112907&clcid=0x41f&culture=tr-tr&country=TR
+sudo rpm -i teams-1.5.00.10453-1.x86_64.rpm 
+sudo dnf localinstall teams-1.5.00.10453-1.x86_64
+
